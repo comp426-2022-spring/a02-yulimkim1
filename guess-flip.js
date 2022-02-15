9eb2885909dfdb1bc571193b51fc7351a0911dcb
Yulim Kim <yulimkim@email.unc.edu>
2022-02-15 17:27:21 -0500

    fix error message?

--- guess-flip.js
+++ guess-flip.js
@@ -8,5 +8,5 @@ if (args.call == "heads" || args.call == "tails") {
     console.log(flipACoin(call))
 }
 else {
-    console.log("Error: no input.\nUsage: node guess-flip.js --call=[heads|tails]")
+    console.log("Error: no input.\nUsage: node guess-flip --call=[heads|tails]")
 }
