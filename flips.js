dd686e5fe477f6c6c9f9cc0e8daec38920a6505d
Yulim Kim <yulimkim@email.unc.edu>
2022-02-15 16:59:15 -0500

    flips with minimist added

--- flips.js
+++ flips.js
@@ -1,4 +1,9 @@
 // Import the coinFlip function from your coin.mjs file
 import { coinFlips } from "./modules/coin.mjs";
+import minimist from 'minimist'
 // Call the coinFlip function and put the return into STDOUT
-console.log(coinFlips())
+
+const args = minimist(process.argv.slice(2));
+const num = args.num || 1;
+
+console.log(coinFlips(num))
