82fa0aa3a988af174b0c235e2c315a4ef728eec4
Yulim Kim <yulimkim@email.unc.edu>
2022-02-15 18:00:25 -0500

    flips fix

--- flips.js
+++ flips.js
@@ -4,9 +4,9 @@ import minimist from 'minimist'
 // Call the coinFlip function and put the return into STDOUT
 
 const args = minimist(process.argv.slice(2));
-const num = args.num || 1;
+const number = args.number || 1;
 
-let flips = coinFlips(num)
+let flips = coinFlips(number)
 
 console.log(flips)
 console.log(countFlips(flips))
